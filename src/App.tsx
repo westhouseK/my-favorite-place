a8a1a5338f78c38dde0c6fc3bc92c851bf1221f5
westhouseK <nishiiekazuki@gmail.com>
2022-03-16 23:19:50 +0900

    [add] can show Google map

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import React from 'react';
 
-const App: React.FC = () => <h1>Hello World</h1>;
+const API_KEY = process.env.REACT_APP_API_KEY as string;
+
+const containerStyle = {
+  width: '800px',
+  height: '800px',
+};
+
+const center = {
+  lat: 35.69575,
+  lng: 139.77521,
+};
+
+const App: React.FC = () => (
+  <LoadScript googleMapsApiKey={API_KEY}>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={17}
+    />
+  </LoadScript>
+);
 
 export default App;
